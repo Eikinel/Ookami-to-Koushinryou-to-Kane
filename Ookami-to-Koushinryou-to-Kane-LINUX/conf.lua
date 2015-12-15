511b04a9966116a91b8fa414765d9f5f988b883c
Valentin Pierrel <eikinelyt@gmail.com>
2015-12-15 13:53:14 +0100

    complete conf.lua

--- Ookami-to-Koushinryou-to-Kane-LINUX/conf.lua
+++ Ookami-to-Koushinryou-to-Kane-LINUX/conf.lua
@@ -1,5 +1,11 @@
 function love.conf(t)
+   t.version = "0.9.2"
+   
+   t.window.title = "Ookami to Koushinryou to Kane"
+   t.window.icon = "res/img/bourse.png"
    t.window.width = 1280
    t.window.height = 720
-   t.window.title = "Ookami to Koushinryou to Kane"
+   t.window.borderless = false
+   t.window.resizable = false
+   t.window.fullscreen = false
 end
